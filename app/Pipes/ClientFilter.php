dca917d77131fa10fdd31503c1628416f683a2e4
daniel <lesamessipa@gmail.com>
2024-09-29 23:48:48 +0200

    add api plateform

--- app/Pipes/ClientFilter.php
+++ app/Pipes/ClientFilter.php
@@ -5,7 +5,7 @@ namespace App\Pipes;
 class ClientFilter
 {
     public function handle($query , \Closure $next) {
-        //on effectue la requete si on a le parametre status dans la request
+        //on effectue la requete si on a le parametre client dans la request
         $query->when(request()->has('client'), function ($query) {
             // on effectue le filtre
             $query->where('client_id', request()->query('client'));
